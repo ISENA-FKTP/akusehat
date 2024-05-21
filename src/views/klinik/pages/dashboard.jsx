416de0848f48b49ced701c8042f1bd8f0b4da253
mudinnova <mudinnova15@gmail.com>
2024-05-21 21:20:39 +0800

    dahsboard

--- src/views/klinik/pages/dashboard.jsx
+++ src/views/klinik/pages/dashboard.jsx
@@ -1,4 +1,6 @@
 import Sidebar_Klinik from "../../../components/klinik/sidebar_klinik";
+import React from "react";
+
 
 export default function Dashboard() {
   return (
@@ -6,10 +8,28 @@ export default function Dashboard() {
       {" "}
       <div className="flex">
         <Sidebar_Klinik />
-        <div className="p-7">
-          <h1 className="text-2xl font-semibold">Dashboard</h1>
+        <div className="flex-1 relative">
+          <div className="absolute inset-0">
+            <img 
+              src="/backdash.png" 
+              alt="Background" 
+              className="object-cover w-full h-full"
+            />
+          </div>
+          <div className="relative z-10 flex items-center justify-center h-full">
+          <div className="text-center text-height">
+            <h1 className="text-white text-5xl font-poppins font-bold">
+              Selamat Datang
+            </h1>
+            <div className="text-white">
+            <h2>ISENA-FKTP</h2>
+              {/* Add your dashboard content here */}
+              </div>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
+
     </>
   );
 }
