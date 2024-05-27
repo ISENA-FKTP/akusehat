3d8b5388a0171ec3e0f3dcb49efcd2c8e7e3245c
RusdyZain <ryanfajri28@gmail.com>
2024-05-27 10:38:41 +0800

    Progres

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
-import Statistik from "./views/statistik";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import DataSakitPolisi from "./views/statistik/pages/data-sakit-polisi";
-import DataPengunjungKlinik from "./views/statistik/pages/data-pengunjung-klinik";
-import DataObatKlinik from "./views/statistik/pages/data-obat-klinik";
+
 import Dashboard from "./views/klinik/pages/dashboard";
 import Administrasi from "./views/klinik/pages/administrasi";
 import Dokter from "./views/klinik/pages/dokter";
@@ -14,13 +11,6 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Statistik />} />
-        <Route path="/data-sakit-polisi" element={<DataSakitPolisi />} />
-        <Route
-          path="/data-pengunjung-klinik"
-          element={<DataPengunjungKlinik />}
-        />
-        <Route path="/data-obat-klinik" element={<DataObatKlinik />} />
         <Route path="/" element={<Klinik />} />
         <Route path="/administrasi" element={<Administrasi />} />
         <Route path="/dashboard" element={<Dashboard />} />
