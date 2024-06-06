926b1dc8b2a9d3cce3971bab769b47000b8b5d6b
M. Rosyadi Aulawi <dadi15122001@gmail.com>
2024-06-07 02:44:39 +0800

    menambahkan pengingat

--- src/views/apoteker/pages/Pengingat.jsx
+++ src/views/apoteker/pages/Pengingat.jsx
@@ -3,27 +3,53 @@ import { FaEdit } from 'react-icons/fa';
 import Sidebar from "../../../components/apotik/sidebar";
 import Header from "../../../components/header";
 
-const DashboardApotek = () => {
+const Pengingat = () => {
   const [medicines, setMedicines] = useState([]);
-  const [sortedMedicines, setSortedMedicines] = useState([]);
+  const [expiringMedicines, setExpiringMedicines] = useState([]);
+  const [lowStockMedicines, setLowStockMedicines] = useState([]);
   const [sortBy, setSortBy] = useState('most');
   const [searchTerm, setSearchTerm] = useState('');
   const [isEditModalOpen, setIsEditModalOpen] = useState(false);
   const [currentMedicine, setCurrentMedicine] = useState(null);
 
-  const categories = ['Obat Cair', 'Obat Padat']; // Kategori yang tersedia
-  const types = ["Tablet", "Syrup"]; // Jenis obat yang tersedia
+  const categories = ['Obat Cair', 'Obat Padat'];
+  const types = ['Tablet', 'Syrup'];
 
   useEffect(() => {
-    // Fetch data saat komponen dimount
     fetch('/data/medicine.json')
       .then(response => response.json())
       .then(data => {
         setMedicines(data);
-        sortMedicines(data); // Mengurutkan data saat pertama kali diambil
+        filterMedicines(data);
       })
       .catch(error => console.error('Error fetching medicines:', error));
-  }, [sortBy]); // Diaktifkan kembali saat sortBy berubah
+  }, []);
+
+  useEffect(() => {
+    filterMedicines(medicines);
+  }, [sortBy, medicines]);
+
+  const filterMedicines = (medicinesToFilter) => {
+    const now = new Date();
+    const sixMonthsLater = new Date();
+    sixMonthsLater.setMonth(sixMonthsLater.getMonth() + 6);
+
+    const expiring = medicinesToFilter.filter(medicine => {
+      const expiryDate = new Date(medicine.expiryDate);
+      return expiryDate <= sixMonthsLater;
+    });
+
+    const lowStock = medicinesToFilter.filter(medicine => medicine.quantity < 100);
+
+    setExpiringMedicines(sortMedicines(expiring));
+    setLowStockMedicines(sortMedicines(lowStock));
+  };
+
+  const sortMedicines = (medicinesToSort) => {
+    return sortBy === 'most'
+      ? [...medicinesToSort].sort((a, b) => b.quantity - a.quantity)
+      : [...medicinesToSort].sort((a, b) => a.quantity - b.quantity);
+  };
 
   const handleSortChange = (e) => {
     setSortBy(e.target.value);
@@ -56,18 +82,14 @@ const DashboardApotek = () => {
       medicine.medicineName === currentMedicine.medicineName ? currentMedicine : medicine
     );
     setMedicines(updatedMedicines);
-    sortMedicines(updatedMedicines);
     closeEditModal();
   };
 
-  const sortMedicines = (medicinesToSort) => {
-    const sorted = sortBy === 'most'
-      ? [...medicinesToSort].sort((a, b) => b.quantity - a.quantity)
-      : [...medicinesToSort].sort((a, b) => a.quantity - b.quantity);
-    setSortedMedicines(sorted);
-  };
+  const filteredExpiringMedicines = expiringMedicines.filter(medicine =>
+    medicine.medicineName.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
-  const filteredMedicines = sortedMedicines.filter(medicine =>
+  const filteredLowStockMedicines = lowStockMedicines.filter(medicine =>
     medicine.medicineName.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
@@ -78,14 +100,14 @@ const DashboardApotek = () => {
       </div>
       <div className="flex-1">
         <Header
-          title="Dashboard Apotek"
+          title="Pengingat Obat"
           userName="Rifki Rusdi Satma Putra"
           userStatus="Apoteker"
           profilePicture="/logo.png"
         />
         <div className="container mx-auto">
-          <h1 className="text-2xl font-bold mt-4 mb-2">Data Seluruh Obat Apotek</h1>
-          <h2 className="text-xl font-semibold mb-4 text-secondary-700">Seluruh data terkait obat di apotek</h2>
+          <h1 className="text-2xl font-bold mt-4 mb-2">Pengingat Obat Kadaluarsa dan Stok Kurang dari 100</h1>
+          <h2 className="text-xl font-semibold mb-4 text-secondary-700">Seluruh data terkait obat di apotek yang akan kadaluarsa dalam 6 bulan dan stok kurang dari 100</h2>
           <div className="flex justify-between mb-4">
             <div>
               <label htmlFor="sort">Urutkan berdasarkan:</label>
@@ -104,6 +126,50 @@ const DashboardApotek = () => {
               />
             </div>
           </div>
+
+          <h2 className="text-xl font-semibold mb-4 text-secondary-700">Obat yang Kadaluarsa dalam 6 Bulan</h2>
+          <div className="overflow-x-auto mb-8">
+            <table className="table-auto w-full">
+              <thead>
+                <tr>
+                  <th className="border px-4 py-2 bg-warning-400">Nama Obat</th>
+                  <th className="border px-4 py-2 bg-warning-400">Jumlah</th>
+                  <th className="border px-4 py-2 bg-warning-400">Kategori</th>
+                  <th className="border px-4 py-2 bg-warning-400">Jenis</th>
+                  <th className="border px-4 py-2 bg-warning-400">Entry Date</th>
+                  <th className="border px-4 py-2 bg-warning-400">Expiry Date</th>
+                  <th className="border px-4 py-2 bg-warning-400">Harga</th>
+                  <th className="border px-4 py-2 bg-warning-400">Aksi</th>
+                </tr>
+              </thead>
+              <tbody>
+                {filteredExpiringMedicines.length === 0 ? (
+                  <tr>
+                    <td colSpan="8" className="text-center py-4">Tidak ada data obat yang sesuai</td>
+                  </tr>
+                ) : (
+                  filteredExpiringMedicines.map((medicine, index) => (
+                    <tr key={index} className={index % 2 === 0 ? 'bg-warning-100' : 'bg-warning-200'}>
+                      <td className="border px-4 py-2">{medicine.medicineName}</td>
+                      <td className="border px-4 py-2">{medicine.quantity}</td>
+                      <td className="border px-4 py-2">{medicine.category}</td>
+                      <td className="border px-4 py-2">{medicine.type}</td>
+                      <td className="border px-4 py-2">{medicine.entryDate}</td>
+                      <td className="border px-4 py-2">{medicine.expiryDate}</td>
+                      <td className="border px-4 py-2">Rp{medicine.price}</td>
+                      <td className="border px-4 py-2 flex items-center justify-center">
+                        <button onClick={() => openEditModal(medicine)} className="p-2 rounded-xl bg-primary-300">
+                          <FaEdit />
+                        </button>
+                      </td>
+                    </tr>
+                  ))
+                )}
+              </tbody>
+            </table>
+          </div>
+
+          <h2 className="text-xl font-semibold mb-4 text-secondary-700">Obat dengan Stok Kurang dari 100</h2>
           <div className="overflow-x-auto">
             <table className="table-auto w-full">
               <thead>
@@ -119,46 +185,53 @@ const DashboardApotek = () => {
                 </tr>
               </thead>
               <tbody>
-                {filteredMedicines.map((medicine, index) => (
-                  <tr key={index} className={index % 2 === 0 ? 'bg-warning-100' : 'bg-warning-200'}>
-                    <td className="border px-4 py-2">{medicine.medicineName}</td>
-                    <td className="border px-4 py-2">{medicine.quantity}</td>
-                    <td className="border px-4 py-2">{medicine.category}</td>
-                    <td className="border px-4 py-2">{medicine.type}</td>
-                    <td className="border px-4 py-2">{medicine.entryDate}</td>
-                    <td className="border px-4 py-2">{medicine.expiryDate}</td>
-                    <td className="border px-4 py-2">Rp{medicine.price}</td>
-                    <td className="border px-4 py-2 flex items-center justify-center">
-                      <button onClick={() => openEditModal(medicine)} className="p-2 rounded-xl bg-primary-300">
-                        <FaEdit />
-                      </button>
-                    </td>
+                {filteredLowStockMedicines.length === 0 ? (
+                  <tr>
+                    <td colSpan="8" className="text-center py-4">Tidak ada data obat yang sesuai</td>
                   </tr>
-                ))}
+                ) : (
+                  filteredLowStockMedicines.map((medicine, index) => (
+                    <tr key={index} className={index % 2 === 0 ? 'bg-warning-100' : 'bg-warning-200'}>
+                      <td className="border px-4 py-2">{medicine.medicineName}</td>
+                      <td className="border px-4 py-2">{medicine.quantity}</td>
+                      <td className="border px-4 py-2">{medicine.category}</td>
+                      <td className="border px-4 py-2">{medicine.type}</td>
+                      <td className="border px-4 py-2">{medicine.entryDate}</td>
+                      <td className="border px-4 py-2">{medicine.expiryDate}</td>
+                      <td className="border px-4 py-2">Rp{medicine.price}</td>
+                      <td className="border px-4 py-2 flex items-center justify-center">
+                        <button onClick={() => openEditModal(medicine)} className="p-2 rounded-xl bg-primary-300">
+                          <FaEdit />
+                        </button>
+                      </td>
+                    </tr>
+                  ))
+                )}
               </tbody>
             </table>
           </div>
         </div>
-        {isEditModalOpen && (
-          <EditModal
-            medicine={currentMedicine}
-            handleEditChange={handleEditChange}
-            handleSaveChanges={handleSaveChanges}
-            closeEditModal={closeEditModal}
-            categories={categories}
-            types={types}
-          />
-        )}
       </div>
+
+      {isEditModalOpen && currentMedicine && (
+        <EditMedicineModal
+          medicine={currentMedicine}
+          closeEditModal={closeEditModal}
+          handleEditChange={handleEditChange}
+          handleSaveChanges={handleSaveChanges}
+          categories={categories}
+          types={types}
+        />
+      )}
     </div>
   );
 };
 
-const EditModal = ({ medicine, handleEditChange, handleSaveChanges, closeEditModal, categories, types }) => {
+const EditMedicineModal = ({ medicine, closeEditModal, handleEditChange, handleSaveChanges, categories, types }) => {
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
       <div className="bg-white p-6 rounded-md shadow-md w-1/2 max-h-screen overflow-y-auto">
-        <h2 className="text-2xl mb-4">Edit Obat</h2>
+        <h2 className="text-xl mb-4">Edit Obat</h2>
         <div className="mb-4">
           <label className="block text-sm font-medium text-gray-700">Nama Obat</label>
           <input 
@@ -167,6 +240,7 @@ const EditModal = ({ medicine, handleEditChange, handleSaveChanges, closeEditMod
             value={medicine.medicineName} 
             onChange={handleEditChange} 
             className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-500 focus:ring-opacity-50"
+            readOnly
           />
         </div>
         <div className="mb-4">
@@ -254,4 +328,4 @@ const EditModal = ({ medicine, handleEditChange, handleSaveChanges, closeEditMod
   );
 };
 
-export default DashboardApotek;
+export default Pengingat;
