d0afe89ff62570fe3fd6d94419476acd60cfdf21
RusdyZain <ryanfajri28@gmail.com>
2024-06-16 03:03:53 +0800

    Progress 14

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import axios from "axios";
+
+// Set axios defaults
 axios.defaults.withCredentials = true;
+axios.defaults.baseURL = "https://be-isena-fktp.onrender.com";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
